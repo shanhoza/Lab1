abfef3209bd6dac6f6f672c8b436539ba25f6ae9
Vitaly Sokol <noshimaof@mail.ru>
2021-11-23 16:37:17 +0200

    Lab-1

---
+++ main.py
@@ -0,0 +1,21 @@
+from decimal import *
+from math import factorial
+
+AC = 100
+N = 4
+
+
+def pi(N):
+    pi = Decimal(0)
+    k = 0
+    while k < AC:
+        pi += (Decimal(-1) ** k) * (Decimal(factorial(6 * k)) / ((factorial(k) ** 3) * (factorial(3 * k))) * (
+                13591409 + 545140134 * k) / (640320 ** (3 * k)))
+        k += 1
+    pi = pi * Decimal(10005).sqrt() / 4270934400
+    pi = pi ** (-1)
+    return round(pi, N)
+
+
+getcontext().prec = AC
+print(pi(N), '\n>> Точность:', AC, '\n>> Количество знаков:', N)
